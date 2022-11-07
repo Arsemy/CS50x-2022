bc1f462a14669d651826b1a89312fdbf84e8e304
Arsemy <arsemy2@gmail.com>
2022-11-07 10:16:12 -0300

    feat: address example

--- week04/lecture/address.c
+++ week04/lecture/address.c
@@ -1,10 +1,15 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
+    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
 
-    printf("%p\n", p);
-    printf("%i\n", *p);
+    printf("%i\n", *numbers);
+    printf("%i\n", *(numbers + 1));
+    printf("%i\n", *(numbers + 2));
+    printf("%i\n", *(numbers + 3));
+    printf("%i\n", *(numbers + 4));
+    printf("%i\n", *(numbers + 5));
+    printf("%i\n", *(numbers + 6));
 }
