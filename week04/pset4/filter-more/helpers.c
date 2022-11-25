8c38ba603de671ea3742ca2c518ca332c33ec45e
Arsemy <arsemy2@gmail.com>
2022-11-25 19:35:23 -0300

    feat: add reflect filter

--- week04/pset4/filter-more/helpers.c
+++ week04/pset4/filter-more/helpers.c
@@ -19,6 +19,25 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //get every
+    for (int i = 0; i < height; i++)
+    {
+        int j = 0;
+        RGBTRIPLE tmp;
+        while (j < width - j - 1)
+        {
+            j++;
+            tmp.rgbtBlue = image[i][j].rgbtBlue;
+            tmp.rgbtGreen = image[i][j].rgbtGreen;
+            tmp.rgbtRed = image[i][j].rgbtRed;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][width - j - 1].rgbtBlue = tmp.rgbtBlue;
+            image[i][width - j - 1].rgbtGreen = tmp.rgbtGreen;
+            image[i][width - j - 1].rgbtRed = tmp.rgbtRed;
+        }
+    }
     return;
 }
 
